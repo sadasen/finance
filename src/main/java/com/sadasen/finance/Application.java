71df5753324eb318ef5794341cddaa00a7b0da18
leiys13 <leiys13@icloud.com>
2018-03-08 15:00:17 +0800

    page和启动类

---
+++ src/main/java/com/sadasen/finance/Application.java
@@ -0,0 +1,19 @@
+package com.sadasen.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @date 2018年3月8日
+ * @author lei.ys
+ * @addr company
+ * @desc
+ */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
